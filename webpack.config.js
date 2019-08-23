c1799aff3899249566e558121575eaa54c8adc91
Viresh Shah <viresh.shah@autodesk.com>
2019-08-23 15:23:41 +0530

    Updated packages to latest versions. Replaced UglifyJS with TerserJS.

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,52 @@
-const webpack = require('webpack');
-const pkg = require('./package.json');
-const env = require('yargs').argv.env;
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const pkg = require("./package.json");
+const env = require("yargs").argv.env;
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
+var TerserPlugin = require("terser-webpack-plugin");
 
 let libraryName = pkg.name;
 
 let outputFile, mode;
 
-if (env === 'build') {
-  mode = 'production';
-  outputFile = libraryName + '.min.js';
+if (env === "build") {
+  mode = "production";
+  outputFile = libraryName + ".min.js";
 } else {
-  mode = 'development';
-  outputFile = libraryName + '.js';
+  mode = "development";
+  outputFile = libraryName + ".js";
 }
 
 module.exports = {
-	entry: [
-		__dirname + '/src/index.js',
-    ],
-    mode: mode,
-	output: {
-		path: __dirname + '/dist',
-        filename: outputFile,
-        library: libraryName,
-        libraryTarget: 'umd',
-        umdNamedDefine: true
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader'
-			}
-		],
-	},
-	plugins: [
+  entry: [__dirname + "/src/index.js"],
+  mode: mode,
+  output: {
+    path: __dirname + "/dist",
+    filename: outputFile,
+    library: libraryName,
+    libraryTarget: "umd",
+    umdNamedDefine: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+  plugins: [
     new BundleAnalyzerPlugin({
-      analyzerMode: 'static',
+      analyzerMode: "static",
       openAnalyzer: false
     })
   ],
-	optimization: {
-		minimize: true,
-		minimizer: [
-			new UglifyJsPlugin({
-				sourceMap: true
-			})
-		]
-	}
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        sourceMap: true
+      })
+    ]
+  }
 };
